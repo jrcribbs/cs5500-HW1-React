0e82af71b13efb1140ba5c87bea008b346652f4b
JR Cribbs <jrd.cribbs@gmail.com>
2022-11-20 13:20:09 -0500

    added my tuits and my likes

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,6 +1,11 @@
 import * as service from "../../services/auth-service"
 import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate, Route} from "react-router-dom";
+import MyTuits from "./my-tuits";
+// import TuitsAndReplies
+//   from "./tuits-and-replies";
+// import Media from "./media";
+import MyLikes from "./my-likes";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -23,6 +28,24 @@ const Profile = () => {
         <h6>@{profile.username}</h6>
         <button onClick={logout}>
           Logout</button>
+        <Link to="/profile/mytuits">
+          Tuits</Link>
+        {/*<Link to="/profile/tuits-and-replies">*/}
+        {/*  Tuits & replies</Link>*/}
+        {/*<Link to="/profile/media">*/}
+        {/*  Media</Link>*/}
+        <Link to="/profile/mylikes">
+          Likes</Link>
+        <Route>
+          <Route path="/mytuits"
+                 element={<MyTuits/>}/>
+          {/*<Route path="/tuits-and-replies"*/}
+          {/*       element={<TuitsAndReplies/>}/>*/}
+          {/*<Route path="/media"*/}
+          {/*       element={<Media/>}/>*/}
+          <Route path="/mylikes"
+                 element={<MyLikes/>}/>
+        </Route>
       </div>
   );
 };
