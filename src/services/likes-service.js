b6c994b0ab498bf8eba1bd55aff58ce6c02f6b84
JR Cribbs <jrd.cribbs@gmail.com>
2022-11-20 13:13:51 -0500

    added findalltuitslikebyuser

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+//  BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "http://localhost:4000"
 const USERS_API = `${BASE_URL}/api/users`;
 
 const api = axios.create({
@@ -9,4 +10,8 @@ const api = axios.create({
 
 export const userTogglesTuitLikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
+    .then(response => response.data);
+
+export const findAllTuitsLikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/likes`)
     .then(response => response.data);
